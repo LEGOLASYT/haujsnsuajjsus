5e2f80af145bc3047222c879a1868c88f3d3e1c5
LEGOLASYT <88878524+LEGOLASYT@users.noreply.github.com>
2021-08-25 10:39:26 +0000

    Add files via upload

---
+++ baslat.bat
@@ -0,0 +1,6 @@
+echo off
+color 0f
+cls
+:legolas
+node main.js
+goto legolas
---
+++ main.js
@@ -0,0 +1,46 @@
+const Discord = require('discord.js-selfbot') // discord.js modülü tanımlıyoruz.
+const client = new Discord.Client() // client tanımalamsı
+const { readdirSync } = require('fs'); // tanımlamalar
+const { join } = require('path'); // tanımlamalar
+
+client.commands= new Discord.Collection(); // komutları alıyoruz
+
+const prefix = "!"
+
+const commandFiles = readdirSync(join(__dirname, "komutlar")).filter(file => file.endsWith(".js")); // Belli bir klasörden belli .js uzantılı dosyaları buluyor.
+
+for (const file of commandFiles) {
+    const command = require(join(__dirname, "komutlar", `${file}`));
+    client.commands.set(command.kod, command); // Komutları Ayarlıyoruz.
+}
+
+client.on("error", console.error);
+
+client.on('ready', () => {
+    client.user.setActivity('https://www.itemsatis.com/profil/157888/teknolegil.html')
+    console.log('Botumuz Aktif')
+});
+
+client.on("message", async message => {
+
+    if(message.author.bot) return;
+
+    if(message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+
+        const command = args.shift().toLowerCase();
+
+        if(!client.commands.has(command)) return message.channel.send(``);
+
+
+        try {
+            client.commands.get(command).run(client, message, args);
+
+        } catch (error){
+            console.error(error);
+        }
+    }
+})
+
+
+client.login("ODc3NTAyOTM4NjMxNzY2MDI2.YRzkTQ.la3MqeTPloeINsjKSGTV4u2d_LE") 
